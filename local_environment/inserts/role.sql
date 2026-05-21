20058247c8089e933ace291d978d36573de85a19
WilliamV <sanchezalvarez124@gmail.com>
2026-05-20 19:55:52 -0500

    feat: ✨ The insertion folder is implemented.

---
+++ local_environment/inserts/role.sql
@@ -0,0 +1,5 @@
+INSERT INTO vota_mas.role
+VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', now(), now(), true, 'ADMINISTRADOR');
+
+INSERT INTO vota_mas.user_role (user_id, role_id)
+VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', '121e1766-2d53-4f3d-a1e1-8b72f3285e16');
---
+++ local_environment/inserts/users.sql
@@ -0,0 +1,2 @@
+INSERT INTO vota_mas.users (user_id, name, surname, email, password)
+VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', 'WILLIAM', 'SANCHEZ', 'prueba@corre.com', '12345');
