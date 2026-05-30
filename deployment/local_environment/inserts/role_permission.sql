fb856de67b7fa1d7a4652319512272fb68bdb98e
williamrep <sanchezalvarez124@gmail.com>
2026-05-30 10:32:43 -0500

    feat(db): ✨ add role_permission relationship table

---
+++ deployment/local_environment/inserts/role_permission.sql
@@ -0,0 +1,10 @@
+INSERT INTO vota_mas.role_permission (role_id, permission_id)
+VALUES
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', '34698c89-3bd1-47a0-b225-6cd843bfb1a5'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', 'eed952cc-f4df-4ba8-944d-284f79fa5001'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', 'ddf47c42-2d13-4979-a5ba-58af17103b3d'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', '697bb69f-2aa1-42af-98c6-885827ffb083'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', '35c2c191-8d17-4dfb-81a3-2137bdf2a981'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', 'e4a82feb-8e54-415c-aa21-572ab5d641a3'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', 'bc02b771-4b33-494f-b629-d98d1c843cf3'),
+('e50411ea-ab77-453c-a75a-3b085f9eaef2', '4b8111c9-3c22-4e8c-babc-f09d5a95ea8f');
--- deployment/local_environment/schemas/1_Tables/06_permission.sql
+++ deployment/local_environment/schemas/1_Tables/06_permission.sql
@@ -2,8 +2,8 @@ CREATE TABLE vota_mas.permission
 (
     permission_id UUID PRIMARY KEY,
     created_at    TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at    TIMESTAMP    NOT NULL DEFAULT now(),
     name          VARCHAR(255) NOT NULL,
     title         VARCHAR(255) NOT NULL,
-    updated_at    TIMESTAMP    NOT NULL DEFAULT now(),
     module_id     UUID         NOT NULL
 );
