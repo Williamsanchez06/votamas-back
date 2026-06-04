6f839e1899386ca4ced4aeeefcddb86b2444eeee
WilliamV <sanchezalvarez124@gmail.com>
2026-06-03 22:51:40 -0500

    feat: ✨ Password validation is implemented upon login.

--- deployment/local_environment/inserts/users.sql
+++ deployment/local_environment/inserts/users.sql
@@ -1,3 +1,3 @@
 INSERT INTO vota_mas.users (user_id, name, surname, email, password)
 VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', 'WILLIAM', 'SANCHEZ', 'prueba@corre.com',
-        '$2a$12$hozaHMUocu6dk.2pElO3W.hpi3q94yNlPiDoRDDLjnSGf2FE30LFC');
+        '$2a$10$KmJRrmq8m6ykeJqy1eNeE..eKnH4uWDRgBCaiMNiu0IVzp.EnDccG');
--- domain/usecase/src/main/java/com/votamas/usecase/login/LoginUseCase.java
+++ domain/usecase/src/main/java/com/votamas/usecase/login/LoginUseCase.java
@@ -2,6 +2,7 @@ package com.votamas.usecase.login;
 
 import com.votamas.model.auth.Login;
 import com.votamas.model.auth.Token;
+import com.votamas.model.auth.gateways.PasswordRepository;
 import com.votamas.model.auth.gateways.UserPermissionRepository;
 import com.votamas.model.auth.gateways.TokenRepository;
 import com.votamas.model.user.gateways.UserRepository;
@@ -14,20 +15,28 @@ public class LoginUseCase {
     private final UserRepository userRepository;
     private final UserPermissionRepository userPermissionRepository;
     private final TokenRepository tokenGateway;
+    private final PasswordRepository passwordRepository;
 
     public Mono<Token> execute(Login login) {
 
         return userRepository.findByEmail(login.email())
                 .switchIfEmpty(Mono.error(new RuntimeException("Usuario no encontrado")))
-                .flatMap(user ->
-                        userPermissionRepository.findPermissionsByUserId(user.id())
-                                .collectList()
-                                .map(userPermissions -> {
-                                    String accessToken = tokenGateway.generateAccessToken(user, userPermissions);
-                                    return Token.builder()
-                                            .accessToken(accessToken)
-                                            .build();
-                                })
-                );
+                .flatMap(user -> {
+
+                    boolean isValidPassword = passwordRepository.matches(login.password(), user.password());
+
+                    if (!isValidPassword) {
+                        return Mono.error(new RuntimeException("Password incorrecto"));
+                    }
+
+                    return userPermissionRepository.findPermissionsByUserId(user.id())
+                            .collectList()
+                            .map(userPermissions -> {
+                                String accessToken = tokenGateway.generateAccessToken(user, userPermissions);
+                                return Token.builder()
+                                        .accessToken(accessToken)
+                                        .build();
+                            });
+                });
     }
 }
