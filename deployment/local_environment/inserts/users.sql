9c2a208ee6000d208098ae45793403be27c3a32c
William Sanchez <sanchezalvarez124@gmail.com>
2026-08-03 20:50:15 -0500

    fix: adjust user insert

--- deployment/local_environment/inserts/users.sql
+++ deployment/local_environment/inserts/users.sql
@@ -1,3 +1,3 @@
 INSERT INTO vota_mas.users (user_id, name, surname, email, password)
-VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', 'WILLIAM', 'SANCHEZ', 'prueba@corre.com',
-        '$2a$10$KmJRrmq8m6ykeJqy1eNeE..eKnH4uWDRgBCaiMNiu0IVzp.EnDccG');
+VALUES ('121e1766-2d53-4f3d-a1e1-8b72f3285e16', 'WILLIAM', 'SANCHEZ', 'sanchezalvarez124@gmail.com',
+        '$2a$10$3F6Th7mWGLGxITRYQDENxeo79HLHuPeOGEDz1bVpOO5PEDk9YnlGK');
